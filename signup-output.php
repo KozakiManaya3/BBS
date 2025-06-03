5bc94bf62544885bec0c9cd66034b7d7f746ea5c
Soyama1127 <2301017@s.asojuku.ac.jp>
2025-06-03 15:05:37 +0900

    カレントタイムスタンプ問題解決

--- signup-output.php
+++ signup-output.php
@@ -1,6 +1,6 @@
 <?php
     $pdo = new PDO('mysql:host=mysql320.phy.lolipop.lan; dbname=LAA1553893-bbs;','LAA1553893','Bbspass');
-    $sql = $pdo->prepare('insert into user values(null, ?, ?, null)');
+    $sql = $pdo->prepare('insert into user values(null, ?, ?, current_timestamp)');
     $sql->execute([$_REQUEST['name'], $_REQUEST['pass']]);
     header("Location: login.php");
 ?>
