5e9df872eb6cd43afa9e976bd8a196819a8f1330
taiga0816 <tai08160715@icloud.com>
2025-06-05 10:23:20 +0900

    postの変更

--- post.php
+++ post.php
@@ -8,8 +8,29 @@ if (trim($comment) === '') {
     exit;
 }
 
-$entry = "$time\t$name\t$comment\n";
-file_put_contents('comments.txt', $entry, FILE_APPEND);
-header("Location: view.php");
-exit;
+try {
+    $pdo=new PDO('mysql:host=mysql320.phy.lolipop.lan;
+                dbname=LAA1553893-bbs;',
+        'LAA1553893',
+        'Bbspass');
+
+    $stmt = $pdo->prepare("INSERT INTO comments (name, comment, created_at) VALUES (:name, :comment, :created_at)");
+    $stmt->execute([
+        ':name' => $name,
+        ':comment' => $comment,
+        ':created_at' => $time
+    ]);
+
+    header("Location: view.php");
+    exit;
+
+} catch (PDOException $e) {
+    echo "データベースエラー: " . $e->getMessage();
+    exit;
+}
+
+// $entry = "$time\t$name\t$comment\n";
+// file_put_contents('comments.txt', $entry, FILE_APPEND);
+// header("Location: view.php");
+// exit;
 ?>
