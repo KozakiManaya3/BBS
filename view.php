d4da1dd219157e38166d5cf507962f5e1450676d
yuki2004831 <ubei138sy@gmail.com>
2025-06-10 12:17:25 +0900

    土下座します

--- view.php
+++ view.php
@@ -34,11 +34,13 @@ try {
                 $name = htmlspecialchars($row['username']);
                 $comment = nl2br(htmlspecialchars($row['content']));
                 echo "<div class='center'>";
+                echo    "<br>";
                 echo "<strong>$name</strong> さん ($time)";
                 echo "</div>";
                 echo "<div class='post'>";
                 echo "$comment <br>";
-                echo "</div><hr>";
+                echo "</div><br><hr>";
+                
             }
         }
 
